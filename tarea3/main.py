27a786e039d58bf423e6834a82afec6f3928f74d
Diego Hernández Herrera <diegohh0411@gmail.com>
2025-04-05 11:46:25 -0600

    Refactor lexer to process characters individually and update token handling; modify test input

--- tarea3/main.py
+++ tarea3/main.py
@@ -5,20 +5,38 @@ from enum import Enum
 class Tokens(str, Enum):
   ENTERO = 'Entero'
   FLOTANTE = 'Flotante'
+  OTRO = 'Otro'
 
 def lexerAritmetico(archivo):
   with open(archivo, 'r') as f:
     contenido = f.read()
-
     tokens: dict[str, Tokens] = []
-    strings = contenido.split()
-    for string in strings:
-      if (enteros.accepts(string)):
-        tokens.append({ string: Tokens.ENTERO })
-      elif (flotantes.accepts(string)):
-        tokens.append({ string: Tokens.FLOTANTE })
+    current_token: string = ""
+
+    for char in contenido:
+      if char.isalnum() or char in ".+-eE":  # Characters that can be part of a number
+                current_token += char
+      else:
+        if current_token:
+          if enteros.accepts(current_token):
+            tokens.append({ current_token: Tokens.ENTERO })
+          elif flotantes.accepts(current_token):
+            tokens.append({ current_token: Tokens.FLOTANTE })
+          current_token = ""
+
+        if not char.isspace():
+          tokens.append({ char: Tokens.OTRO })
+
+
+    if current_token:
+      if enteros.accepts(current_token):
+          tokens.append({current_token: Tokens.ENTERO})
+      elif flotantes.accepts(current_token):
+          tokens.append({current_token: Tokens.FLOTANTE})
+      else:
+          tokens.append({current_token: Tokens.OTRO})
+
 
     print(tokens)
-
 
 lexerAritmetico('tarea3/prueba.txt')
