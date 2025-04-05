d4e9f04c4bd0278c4ea8ba890ffb8efb369de646
Diego Hernández Herrera <diegohh0411@gmail.com>
2025-04-05 12:15:51 -0600

    Add variable token handling to lexer; implement NFA for variables

--- tarea3/main.py
+++ tarea3/main.py
@@ -1,12 +1,14 @@
 from enteros import enteros
 from flotantes import flotantes
 from operadores import operadores
+from variables import variables
 from enum import Enum
 
 class Tokens(str, Enum):
   ENTERO = 'Entero'
   FLOTANTE = 'Flotante'
   OPERADOR = 'Operador'
+  VARIABLE = 'Variable'
   OTRO = 'Otro'
 
 def lexerAritmetico(archivo):
@@ -26,21 +28,19 @@ def lexerAritmetico(archivo):
             tokens.append({ current_token: Tokens.FLOTANTE })
           elif operadores.accepts(current_token):
             tokens.append({current_token: Tokens.OPERADOR })
+          elif variables.accepts(current_token):
+            tokens.append({current_token: Tokens.VARIABLE })
           current_token = ""
-
-        if not char.isspace():
-          tokens.append({ char: Tokens.OTRO })
-
 
     if current_token:
       if enteros.accepts(current_token):
-          tokens.append({current_token: Tokens.ENTERO})
+        tokens.append({current_token: Tokens.ENTERO})
       elif flotantes.accepts(current_token):
-          tokens.append({current_token: Tokens.FLOTANTE})
+        tokens.append({current_token: Tokens.FLOTANTE})
       elif operadores.accepts(current_token):
-          tokens.append({current_token: Tokens.OPERADOR})
-      else:
-          tokens.append({current_token: Tokens.OTRO})
+        tokens.append({current_token: Tokens.OPERADOR})
+      elif variables.accepts(current_token):
+        tokens.append({current_token: Tokens.VARIABLE })
 
     print("TOKEN\t\tTIPO")
     for token in tokens:
---
+++ tarea3/variables.py
@@ -0,0 +1,17 @@
+from automatas import NFA
+
+nfa_states = {0, 1}
+nfa_alphabet = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_")
+nfa_transitions = {
+    0: {
+        **{char: [1] for char in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"}
+    },
+    1: {
+        **{char: [1] for char in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"}
+    }
+}
+nfa_start_state = 0
+nfa_final_states = {1}
+
+nfa = NFA(nfa_states, nfa_alphabet, nfa_transitions, nfa_start_state, nfa_final_states)
+variables = nfa.to_dfa()
